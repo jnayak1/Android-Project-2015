0fb98a8ce2d058df598a51f9440d81725026d360
jnayak1 <jdn4ae@virgnia.edu>
2015-04-10 15:26:33 -0400

    creature skeleton

---
+++ app/src/main/java/jdn4ae/cs2110/virginia/edu/gamepractice/Creature.java
@@ -0,0 +1,18 @@
+package jdn4ae.cs2110.virginia.edu.gamepractice;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by pnayak1 on 4/10/15.
+ */
+public abstract class Creature {
+
+    Bitmap bitmap;
+    float postionX, positionY;
+
+
+    public void move(){
+        
+    }
+
+}
